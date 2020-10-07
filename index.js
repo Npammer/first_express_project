370756738f25ad3c3e901b5774781de793e38d74
npammer <npammer@gmail.com>
2020-10-07 14:19:33 +0200

    done

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
-const members = require("./data/Members");
-const logger = require("./middleware/logger");
+// const logger = require("./middleware/logger");
+const exphbs = require("express-handlebars");
 const app = express();
 const path = require("path");
 const port = 3001;
 
+const members = require("./data/Members");
+
 // init middleware
 // app.use(logger);
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Homepage route
+app.get("/", (req, res) =>
+  res.render("index", { title: "Members APP", members })
+);
 
 app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "about.html"));
@@ -20,18 +31,8 @@ app.get("/contact-us", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "contact-us.html"));
 });
 
-// GET all members
-app.get("/api/members", (req, res) => res.json(members));
-
-// GET single member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some(member => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter(member => member.id === parseInt(req.params.id)));
-  }
-  res.status(400).json({ msg: `No member with id: ${req.params.id}` });
-});
+// API routes
+app.use("/api/members", require("./routes/api/members"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
